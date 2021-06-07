863bfced1c959183aeb7627b152aded705b5c9f5
Vlad Tomsa <tomsavlad90@gmail.com>
2021-06-07 18:45:46 +0300

    Initial Commit

---
+++ hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by Vlad Tomsa on 07.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
